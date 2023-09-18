04118d2d774eca6fd4fb237cd96f7ea5bcbc8f96
chandu-0214 <ncr02141997@gmail.com>
2023-09-18 19:56:45 +0530

    first commit

---
+++ Module-1/index5.js
@@ -0,0 +1,21 @@
+console.log("Classs 5")
+
+// function (declaration, expression, arrow)
+
+
+
+// 1. Function Declaration
+
+
+// task => Add two numbers
+
+function addingTwoNUmbers (number1,number2) {
+    let sumOfTwoNUmbers = number1+number2
+    return sumOfTwoNUmbers
+}                                                // defining function
+
+let result = addingTwoNUmbers(8,25)   
+console.log(result,'result')                            // calling function
+
+
+
