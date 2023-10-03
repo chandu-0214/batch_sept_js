53fabca14cbd82ef5ea7c53ea21c63cea8bf9ff6
chandu-0214 <ncr02141997@gmail.com>
2023-10-03 19:52:58 +0530

    first commit

--- Module-2/basic/index.js
+++ Module-2/basic/index.js
@@ -1,15 +1,42 @@
 console.log("basic")
+let buttonElement = document.getElementById('button')
+let headingElement = document.getElementById('heading')
+let paraElement = document.getElementById('para')
+let buttonContainerElememt = document.getElementById('buttonContainer')
+let newButtonElemet = document.createElement('button')
+
 
 function headingChange(){
-    let headingElement = document.getElementById('heading')
+    buttonElement.textContent="Change Page"
     headingElement.textContent='Heading Changes'
     headingElement.style.color='blue'
     headingElement.style.backgroundColor='yellow'
     headingElement.style.textDecoration='underline'
-    console.log(headingElement)
+
+    paraElement.textContent="Paragraph got accesed by JS"
+    newButtonElemet.textContent="Original Page"
+    newButtonElemet.id='Original Page'
+    buttonContainerElememt.appendChild(newButtonElemet)
 }
 
-headingChange()
+function handleOriginalPage(){
+    console.log('original ')
+    buttonElement.textContent="Click"
+    headingElement.textContent='Basic'
+    headingElement.style.color=''
+    headingElement.style.backgroundColor=''
+    headingElement.style.textDecoration=''
+    paraElement.textContent="This is para"
+    buttonContainerElememt.removeChild(newButtonElemet)
+}
+
+//headingChange()
+
+// adding event Listerners 
+
+buttonElement.addEventListener('click',headingChange)
+
+newButtonElemet.addEventListener('click',handleOriginalPage)
 
 
 
