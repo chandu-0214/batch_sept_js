59649e9adc69dea143b8cafadfced9481367ac30
chandu-0214 <ncr02141997@gmail.com>
2023-10-06 20:01:03 +0530

    first commit

--- Module-2/forms/index.js
+++ Module-2/forms/index.js
@@ -9,50 +9,35 @@ let GenderOthersEle = document.getElementById('GenderOthers')
 let GenderFeMaleEle = document.getElementById('GenderFeMale')
 let GenderMaleEle = document.getElementById('GenderMale')
 let roleEle = document.getElementById('role')
-
+let myFormId = document.getElementById('myForm')
+let nameErrorEl = document.getElementById('nameError')
+console.log(myFormId)
 
 const dataRuntimeForm ={
     name:"",
     email:"",
     contactNo:"",
     dob:"",
-    gender:"",
-    role:""
+    gender:"Male",
+    role:"Full-stack Developer"
 }
 const handleName=(event)=>{
-    console.log(event.target.value)
     dataRuntimeForm.name=event.target.value
-    console.log(dataRuntimeForm)
-
 }
 const handleEmail=(event)=>{
-    console.log(event.target.value)
     dataRuntimeForm.email=event.target.value
-    console.log(dataRuntimeForm)
-
 }
 const handleNumber=(event)=>{
-    console.log(event.target.value)
     dataRuntimeForm.contactNo=event.target.value
-    console.log(dataRuntimeForm)
-
 }
 const handledob=(event)=>{
-    console.log(event.target.value)
     dataRuntimeForm.dob=event.target.value
-    console.log(dataRuntimeForm)
-
 }
 const handleGender=(event)=>{
-    console.log(event.target.value)
     dataRuntimeForm.gender=event.target.value
-    console.log(dataRuntimeForm)
-
 }
 const handlerole=(event)=>{
-    console.log(event.target.value)
     dataRuntimeForm.role=event.target.value
-    console.log(dataRuntimeForm)
 
 }
 // add event listerners to all html elements 
@@ -67,7 +52,65 @@ GenderMaleEle.addEventListener('change',handleGender)
 roleEle.addEventListener('change',handlerole)
 nameEle.addEventListener('change',handleName)
 
+const validation=()=>{
+    // name is greater than 5
+    // email should includes @
+    // contact no. equals to 10
+    // dob should not be empty
 
+    let isnameValid = dataRuntimeForm.name.length>5 
+    let isEmailValid = dataRuntimeForm.email.includes("@")
+    let isContactValid = dataRuntimeForm.contactNo.length===10
+    let isDobValid= dataRuntimeForm.dob !==""
+    console.log(isnameValid,isEmailValid,isContactValid,isDobValid)
+
+    return isnameValid && isEmailValid && isContactValid && isDobValid
+
+}
+const handleformSubmit = (event)=>{
+    event.preventDefault()  // It stops all the default activites which was done by browser
+    const isValid = validation()
+    if(isValid){
+        console.log(dataRuntimeForm)
+        // fetch call
+        alert('Form is Successfully submitted')
+        // below lines used to make the form empty after submitting successfully
+        nameEle.value=''
+        emailEle.value=''
+        numberEle.value=''
+        dobEle.value=''
+        
+
+    }
+    else{
+        console.log("Form is Invalid")
+        alert('Form is Invalid')
+    }
+     
+
+
+}
+myFormId.addEventListener('submit',handleformSubmit)
+
+
+const handleNameValidation=(event)=>{
+    console.log('blur is triggered for name')
+    let valueInTheName = event.target.value
+    let isnameValid= valueInTheName.length<6
+    console.log(isnameValid)
+    if(isnameValid){
+        nameErrorEl.textContent='Name should be greater than 5'
+        nameErrorEl.style.color='red'
+    }else{
+        nameErrorEl.textContent=''
+        nameErrorEl.style.color=''
+    }
+    console.log(valueInTheName)
+
+}
+
+
+nameEle.addEventListener('blur',handleNameValidation)
 
 
 
