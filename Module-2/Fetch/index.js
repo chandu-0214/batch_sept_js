02ca51110be58e645052b5d9f33456329d45ee97
chandu-0214 <ncr02141997@gmail.com>
2023-10-11 20:46:17 +0530

    first commit

--- Module-2/Fetch/index.js
+++ Module-2/Fetch/index.js
@@ -1,1 +1,19 @@
 
+console.log('start')
+// setTimeout(()=>console.log("SetTimeout"),0)
+// // localStorage.setItem("name",'chandu')
+// console.log("End")
+
+let url = 'https://jsonplaceholder.typicode.com/todos/'
+// fetch(url,options)
+// options is a object{},
+
+
+// GET 
+
+fetch(url)
+.then((response)=>response.json())
+.then((data)=>console.log(data))
+.catch((error)=>console.log(error,"error"))
+console.log("end")
+
